da70da2749356276982ae25e90a82fe37321bbad
jeysondiaz <62860099+jeysondiaz@users.noreply.github.com>
2023-03-14 09:56:01 -0500

    Colombian States
     //change this code 
    Line 50, in:  wp-content/plugins/woocommerce/i18n/states.php

---
+++ CO.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Colombian states
+ //colocar en la ruta wp-content/plugins/woocommerce/i18n/states.php
+ *
+ * @author      WooThemes
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     2.0.0
+ */
+	'CO' => array( // Colombian States.
+		'CA' => __( 'Cali', 'woocommerce' ),
+		'JA' => __( 'Jamundi', 'woocommerce' ),
+		'CN' => __( 'Candelaria', 'woocommerce' ),
+		'YU' => __( 'Yumbo', 'woocommerce' ),
+		'PA' => __( 'Palmira', 'woocommerce' ),
+	),
