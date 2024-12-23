09c1b60a9e491d0dc80160edfac7bdbc563c80de
jeysondiaz <62860099+jeysondiaz@users.noreply.github.com>
2024-12-23 11:18:08 -0500

    Update CO.php
    Update for 9.5.1 woocommerce version
    
    change problem with arrow function

--- CO.php
+++ CO.php
@@ -1,12 +1,29 @@
 <?php
 /**
+ * Countries
+ *
+ * Returns an array of countries and codes.
+ * Country codes and names should follow the Unicode CLDR recommendation (https://cldr.unicode.org/translation/displaynames/countryregion-territory-names).
+ *
+ * See https://github.com/unicode-org/cldr/blob/master/common/subdivisions/en.xml
+ *
+ * @package WooCommerce\i18n
+ * @version 3.8.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+
+/**
  * Colombian states
 colocar en la ruta wp-content/plugins/woocommerce/i18n/states.php
  */
-	'CO' => array( // Colombian States.
+ return array(
+	'CO' => array( 
 		'CA' => __( 'Cali', 'woocommerce' ),
 		'JA' => __( 'Jamundi', 'woocommerce' ),
 		'CN' => __( 'Candelaria', 'woocommerce' ),
 		'YU' => __( 'Yumbo', 'woocommerce' ),
 		'PA' => __( 'Palmira', 'woocommerce' ),
 	),
+	);
